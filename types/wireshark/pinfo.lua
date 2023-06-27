18fc8a1136edd8c12a93ade7c18950b177955aef
software232 <software232@jjsensor.com>
2023-06-27 11:00:53 +0800

    add more types

---
+++ types/wireshark/pinfo.lua
@@ -0,0 +1,85 @@
+--- @meta
+
+--- @class CAddress
+
+Address = {
+    ---comment
+    ---@param hostname string
+    ---@return CAddress
+    ip = function(hostname)
+    end,
+    ---comment
+    ---@param hostname string
+    ---@return CAddress
+    ipv6 = function(hostname)
+    end,
+    ---comment
+    ---@param eth string
+    ---@return CAddress
+    ether = function(eth)
+    end,
+}
+
+--- @class CColumn
+--- @field clear fun()
+--- @field set fun(text: string)
+--- @field append fun(text: string)
+--- @field prepend fun(text: string)
+--- @field fence fun()
+--- @field clear_fence fun()
+
+--- @class CColumns
+
+--- @class CNSTime
+--- @operator call: any
+--- @field tonumber fun(): number
+--- @operator add: CNSTime
+--- @operator sub: CNSTime
+--- @operator unm: CNSTime
+--- @field secs number
+--- @field nsecs number
+
+NSTime = {
+    ---comment
+    ---@param seconds? number
+    ---@param nseconds? number
+    ---@return CNSTime
+    new = function(seconds, nseconds)
+    end
+}
+
+--- @class CPinfo
+--- @field visited boolean
+--- @field number boolean
+--- @field len boolean
+--- @field caplen boolean
+--- @field abs_ts boolean
+--- @field rel_ts boolean
+--- @field delta_ts boolean
+--- @field delta_dis_ts boolean
+--- @field curr_proto CProto
+--- @field desegment_len boolean
+--- @field desegment_offset boolean
+--- @field fragmented boolean
+--- @field in_error_pkt boolean
+--- @field match_uint boolean
+--- @field match_string boolean
+--- @field port_type boolean
+--- @field src_port boolean
+--- @field dst_port boolean
+--- @field dl_src boolean
+--- @field dl_dst boolean
+--- @field net_src boolean
+--- @field net_dst boolean
+--- @field src boolean
+--- @field dst boolean
+--- @field p2p_dir boolean
+--- @field match boolean
+--- @field columns boolean
+--- @field cols boolean
+--- @field private boolean
+--- @field hi boolean
+--- @field lo boolean
+--- @field conversation boolean
+
+--- @class PrivateTable
